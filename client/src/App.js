081aff6fe411d1a042d81cbe492c8516d5165f85
Sameer Jha <sameerjha236@gmail.com>
2023-06-13 14:22:09 +0530

    Adding styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [sor, setSor] = useState("");
   const [data, setData] = useState([]);
+
   const submitHandler = () => {
     axios
       .post("http://localhost:3001/api/location", {
@@ -21,27 +22,32 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Working</h1>
+      <h1 className="title">Practo Working</h1>
       <form>
-        <label htmlFor="sor">Location 1</label>
+        <label htmlFor="sor" className="label">
+          Location 1
+        </label>
         <input
           type="text"
           id="sor"
+          className="input"
           value={sor}
           onChange={(e) => setSor(e.target.value)}
         />
-        <button type="button" onClick={submitHandler}>
+        <button type="button" className="btn" onClick={submitHandler}>
           Submit
         </button>
       </form>
-      {data &&
-        data.map((loc, ind) => {
-          return (
-            <h4 key={ind}>
-              {loc.location} = {loc.distance}
-            </h4>
-          );
-        })}
+      <div className="values">
+        {data &&
+          data.map((loc, ind) => {
+            return (
+              <div key={ind} className="result">
+                {loc.location} = {loc.distance}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
